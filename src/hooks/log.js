ac489651461f8c91823ea9fd14f2695ca2f0afdb
fezproof <stevebob1997@hotmail.com>
2018-12-15 14:53:15 +0800

    Generated feathers app

---
+++ src/hooks/log.js
@@ -0,0 +1,22 @@
+// A hook that logs service method before, after and error
+// See https://github.com/winstonjs/winston for documentation
+// about the logger.
+const util = require('util');
+const logger = require('../logger');
+
+// To see more detailed messages, uncomment the following line:
+// logger.level = 'debug';
+
+module.exports = () => (context) => {
+  // This debugs the service call and a stringified version of the hook context
+  // You can customize the message (and logger) to your needs
+  logger.debug(`${context.type} app.service('${context.path}').${context.method}()`);
+
+  if (typeof context.toJSON === 'function' && logger.level === 'debug') {
+    logger.debug('Hook Context', util.inspect(context, { colors: false }));
+  }
+
+  if (context.error && !context.result) {
+    logger.error(context.error.stack);
+  }
+};
