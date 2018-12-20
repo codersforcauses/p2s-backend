15e316947cd9b8feda1358351880c48d3e6c3966
fezproof <stevebob1997@hotmail.com>
2018-12-20 16:58:01 +0800

    added auth back in to admin

--- src/services/admin/admin.hooks.js
+++ src/services/admin/admin.hooks.js
@@ -1,8 +1,8 @@
-// const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authenticate } = require('@feathersjs/authentication').hooks;
 
 module.exports = {
   before: {
-    all: [], // Add authenticate when done
+    all: [authenticate('jwt')],
     find: [],
     get: [],
     create: [
