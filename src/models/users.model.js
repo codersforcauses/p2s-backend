324ab891dc923033404a23efbed6f9b16e22a198
fezproof <stevebob1997@hotmail.com>
2018-12-31 12:37:00 +0800

    Flattened user schema

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -7,68 +7,100 @@ const mongoose = require('mongoose');
 module.exports = (app) => {
   const mongooseClient = app.get('mongooseClient');
   const { ObjectId } = mongoose.Schema.Types;
-  const users = new mongooseClient.Schema({
-    email: {
-      type: String,
-      unique: true,
-      lowercase: true,
-      required: true,
-    },
-    password: {
-      type: String,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    contact: {
-      mobile: {
+  const users = new mongooseClient.Schema(
+    {
+      email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: true,
+      },
+      password: {
+        type: String,
+      },
+      name: {
         type: String,
         required: true,
       },
-      emergencyNum: {
+      contact: {
+        mobile: {
+          type: String,
+          required: true,
+        },
+        emergencyNum: {
+          type: String,
+          required: true,
+        },
+      },
+      gender: {
         type: String,
+        enum: ['Male', 'Female', 'Other'],
         required: true,
       },
+      ethnicity: {
+        type: String,
+        required: true, // TODO add ethnicity options
+      },
+      DOB: {
+        type: Date,
+        required: true,
+      },
+      darktheme: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+      student: {
+        type: ObjectId,
+        ref: 'student',
+      },
+      coach: {
+        is: {
+          type: Boolean,
+          default: false,
+        },
+        qualifications: {
+          policeClearance: {
+            type: Boolean,
+            require: true,
+            default: false,
+          },
+          WWC: {
+            type: Boolean,
+            require: true,
+            default: false,
+          },
+          medClearance: {
+            type: Boolean,
+            require: true,
+            default: false, // TODO link to file server
+          },
+        },
+      },
+      manager: {
+        is: {
+          type: Boolean,
+          default: false,
+        },
+        coaches: [
+          {
+            type: ObjectId,
+            required: true,
+            ref: 'users',
+          },
+        ],
+      },
+      admin: {
+        is: {
+          type: Boolean,
+          default: false,
+        },
+      },
     },
-    gender: {
-      type: String,
-      enum: ['Male', 'Female', 'Other'],
-      required: true,
+    {
+      timestamps: true,
     },
-    ethnicity: {
-      type: String,
-      required: true, // TODO add ethnicity options
-    },
-    DOB: {
-      type: Date,
-      required: true,
-    },
-    darktheme: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    student: {
-      type: ObjectId,
-      ref: 'student',
-    },
-    coach: {
-      type: ObjectId,
-      ref: 'coach',
-    },
-    manager: {
-      type: ObjectId,
-      ref: 'manager',
-    },
-    admin: {
-      type: ObjectId,
-      ref: 'admin',
-    },
-  },
-  {
-    timestamps: true,
-  });
+  );
 
   return mongooseClient.model('users', users);
 };
