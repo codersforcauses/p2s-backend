079fb1f768ae2e49063bb61e3199c75177fec928
fezproof <stevebob1997@hotmail.com>
2018-12-31 15:53:07 +0800

    Updated report schema

--- src/models/reports.model.js
+++ src/models/reports.model.js
@@ -2,72 +2,30 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-const mongoose = require('mongoose');
 
 module.exports = (app) => {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
-  const { ObjectId } = mongoose.Schema.Types;
   const reports = new Schema(
     {
-      user: {
-        type: ObjectId,
-        ref: 'users',
-        required: true,
-      },
-      session: {
-        type: ObjectId,
-        ref: 'session',
-        required: true,
-      },
       attended: {
-        type: Boolean,
+        type: String,
+        enum: [
+          'Present',
+          'SchoolAbsent',
+          'SchoolAttended',
+          'TeacherResricted',
+          'TeacherRescrictedExam',
+        ],
         required: true,
       },
-      studentReport: {
-        matrixResults: [
-          {
-            type: Number,
-            default: 1,
-            required: true,
-          },
-        ],
-        comment: {
-          type: String,
+      matrixResults: [
+        {
+          type: Number,
+          default: 1,
           required: true,
         },
-      },
-      activities: [
-        {
-          activity: {
-            type: ObjectId,
-            ref: 'activities',
-            required: true,
-          },
-          run: {
-            type: Boolean,
-            required: true,
-          },
-        },
       ],
-      focus: {
-        type: String,
-      },
-      improvement: {
-        type: String,
-      },
-      approach: {
-        type: String,
-      },
-      restrictions: {
-        type: String,
-      },
-      setbacks: {
-        type: String,
-      },
-      numStaff: {
-        type: Number,
-      },
     },
     {
       timestamps: true,
