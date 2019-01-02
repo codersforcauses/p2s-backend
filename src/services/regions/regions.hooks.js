bfe1a55fe249221194eaf904441b2c36b4546df9
fezproof <stevebob1997@hotmail.com>
2019-01-02 20:45:48 +0800

    Unless admin, users are limited to there region

--- src/services/regions/regions.hooks.js
+++ src/services/regions/regions.hooks.js
@@ -1,10 +1,30 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const { iff, isProvider } = require('feathers-hooks-common');
+
+const { Forbidden } = require('@feathersjs/errors');
+
 const permission = require('../../hooks/permission');
 
 module.exports = {
   before: {
-    all: [authenticate('jwt')],
+    all: [
+      authenticate('jwt'),
+      iff(isProvider('external'), [
+        iff(context => !context.params.user.admin.is, [
+          (context) => {
+            const query = context.params.query || {};
+            if (context.params.user.region) {
+              query.region = context.params.user.region;
+            } else {
+              throw new Forbidden('You dont belong to that region');
+            }
+            context.params.query = query;
+            return context;
+          },
+        ]),
+      ]),
+    ],
     find: [],
     get: [],
     create: [permission({ roles: 'admin' })],
