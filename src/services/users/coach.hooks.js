55d7949d87dafeecd144c8cc13fafa08788c9372
fezproof <stevebob1997@hotmail.com>
2019-01-02 18:19:48 +0800

    Ensure coach is coach only on creation

--- src/services/users/coach.hooks.js
+++ src/services/users/coach.hooks.js
@@ -1,6 +1,6 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const { hashPassword, protect } = require('@feathersjs/authentication-local').hooks;
-const { discardQuery } = require('feathers-hooks-common');
+const { discardQuery, alterItems } = require('feathers-hooks-common');
 
 const permission = require('../../hooks/permission');
 
@@ -9,7 +9,19 @@ module.exports = {
     all: [authenticate('jwt'), discardQuery('admin', 'manager')],
     find: [permission({ roles: ['admin', 'manager', 'coach'] })],
     get: [permission({ roles: ['admin', 'manager', 'coach'] })],
-    create: [permission({ roles: ['admin', 'manager'] }), hashPassword()],
+    create: [
+      permission({ roles: ['admin', 'manager'] }),
+      hashPassword(),
+      alterItems((rec) => {
+        delete rec.admin;
+        delete rec.manager;
+        if (rec.coach) {
+          rec.coach.is = true;
+        } else {
+          rec.coach = { is: true };
+        }
+      }),
+    ],
     update: [permission({ roles: ['admin', 'manager'] }), hashPassword()],
     patch: [permission({ roles: ['admin', 'manager'] }), hashPassword()],
     remove: [permission({ roles: ['admin', 'manager'] })],
