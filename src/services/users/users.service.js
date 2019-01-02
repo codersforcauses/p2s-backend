c78c181a3f5d704aa105febea0bbec1063f12b22
fezproof <stevebob1997@hotmail.com>
2019-01-02 16:02:35 +0800

    Generate god user added common hooks library

--- src/services/users/users.service.js
+++ src/services/users/users.service.js
@@ -1,5 +1,7 @@
 // Initializes the `users` service on path `/users`
 const createService = require('feathers-mongoose');
+const logger = require('../..//logger.js');
+
 const createModel = require('../../models/users.model');
 const hooks = require('./users.hooks');
 
@@ -19,4 +21,32 @@ module.exports = (app) => {
   const service = app.service('users');
 
   service.hooks(hooks);
+
+  if (process.env.NODE_ENV !== 'production') {
+    service
+      .create({
+        email: 'super@admin.god',
+        password: 'Qwerty123',
+        name: {
+          first: 'The one',
+          last: 'GOD',
+        },
+        mobile: '0000000000',
+        emergencyNum: '0000000000',
+        gender: 'Other',
+        ethnicity: 'Other',
+        DOB: '01.01.1901',
+        darktheme: true,
+        admin: {
+          is: true,
+        },
+      })
+      .catch((err) => {
+        if (err.code === 409) {
+          logger.info('GOD is good');
+        } else {
+          throw new Error('GOD is dead\n'.concat(err));
+        }
+      });
+  }
 };
