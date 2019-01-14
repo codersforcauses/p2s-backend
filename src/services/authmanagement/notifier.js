ce6247d43c1790459f0b7bc38902496a533780f9
Luke Stagoll <lukestagoll16@gmail.com>
2019-01-14 15:56:03 +0800

    Added ability to change password

--- src/services/authmanagement/notifier.js
+++ src/services/authmanagement/notifier.js
@@ -2,31 +2,29 @@
 const errors = require('@feathersjs/errors');
 
 module.exports = (app) => {
-  function getLink(type, hash) {
+  const getLink = (type, hash) => {
     const url = 'http://localhost:3030/'.concat(type, '?token=', hash);
     return url;
-  }
+  };
 
-  function sendEmail(email) {
-    return app.service('mailer').create(email).then((result) => {
+  const sendEmail = (email) => {
+    app.service('mailer').create(email).then((result) => {
       console.log('Sent email', result);
     }).catch((err) => {
       console.log('Error sending email', err);
     });
-  }
+  };
 
-  function getEmail(user, subjectData, htmlData) {
-    return {
-      from: process.env.SMTP_USER,
-      to: user.email,
-      subject: subjectData,
-      html: htmlData,
-    };
-  }
+  const getEmail = (user, subjectData, htmlData) => ({
+    from: process.env.SMTP_USER,
+    to: user.email,
+    subject: subjectData,
+    html: htmlData,
+  });
 
 
   return {
-    notifier: (type, user) => { // , notifierOptions) {
+    notifier: (type, user) => {
       let tokenLink;
       let email;
       switch (type) {
@@ -42,21 +40,22 @@ module.exports = (app) => {
 
       case 'sendResetPwd':
         tokenLink = getLink('reset', user.resetToken);
-        email = {};
+        email = getEmail(user, 'Reset Password', tokenLink);
         return sendEmail(email);
 
       case 'resetPwd':
         tokenLink = getLink('reset', user.resetToken);
-        email = {};
+        email = getEmail(user, 'Your password was reset', tokenLink);
         return sendEmail(email);
 
       case 'passwordChange':
-        email = {};
+        tokenLink = getLink('verifyChanges', user.verifyToken);
+        email = getEmail(user, 'Your account was changed. Please verify the changes', tokenLink);
         return sendEmail(email);
 
       case 'identityChange':
         tokenLink = getLink('verifyChanges', user.verifyToken);
-        email = {};
+        email = getEmail(user, 'Your account was changed. Please verify the changes', tokenLink);
         return sendEmail(email);
 
       default:
