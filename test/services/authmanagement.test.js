e455efe10b681349ab96cef92140fae225cb874c
Luke Stagoll <lukestagoll16@gmail.com>
2019-01-29 14:42:29 +0800

    adjusted the authmanagement test

--- test/services/authmanagement.test.js
+++ test/services/authmanagement.test.js
@@ -1,8 +1,10 @@
+const authManagement = require('feathers-authentication-management');
+const notifier = require('../../src/services/authmanagement/notifier');
 const app = require('../../src/app');
 
 describe('\'authmanagement\' service', () => {
   it('registered the service', () => {
-    const service = app.service('authmanagement');
-    expect(service).toBeTruthy();
+    const configure = app.configure(authManagement(notifier(app)));
+    expect(configure).toBeTruthy();
   });
 });
