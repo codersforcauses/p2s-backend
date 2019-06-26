3254c8d814b81036267b305d191c50cbce6a77a6
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-06-26 15:18:14 +0800

    Readded changes to up to date branch

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -1,8 +1,29 @@
 const app = require('../../src/app');
 
-describe('\'users\' service', () => {
-  it('registered the service', () => {
+describe('\'user\' service', () => {
+  it('registered the users service', () => {
     const service = app.service('users');
+    expect(service).toBeTruthy();
+  });
+});
+
+describe('\'admin\' service', () => {
+  it('registered the admin service', () => {
+    const service = app.service('admin');
+    expect(service).toBeTruthy();
+  });
+});
+
+describe('\'manager\' service', () => {
+  it('registered the manager service', () => {
+    const service = app.service('manager');
+    expect(service).toBeTruthy();
+  });
+});
+
+describe('\'coach\' service', () => {
+  it('registered the coach service', () => {
+    const service = app.service('coach');
     expect(service).toBeTruthy();
   });
 });
