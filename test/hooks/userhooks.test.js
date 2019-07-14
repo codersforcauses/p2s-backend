9b9b50be19029fbe7489b3c8df602df03edda57e
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-07-14 15:54:03 +0800

    Userhook testing update

--- test/hooks/userhooks.test.js
+++ test/hooks/userhooks.test.js
@@ -11,19 +11,15 @@ describe('\'userhooks\' hook', () => {
       app = feathers();
       app.use('/verifyToken', {
         async find(context) {
-          return context;
-        },
-        async get(context) {
-          return context;
+          return hasVerifyToken(context);
         },
       });
 
-      app.service('verifyToken').hooks({
-        before: {
-          find: hasVerifyToken(),
-          get: hasVerifyToken(),
-        },
-      });
+      // app.service('verifyToken').hooks({
+      //   before: {
+      //     find: hasVerifyToken(),
+      //   },
+      // });
     });
 
     it('Should return true for a present verifyToken', async () => {
@@ -35,28 +31,6 @@ describe('\'userhooks\' hook', () => {
       const result = await app.service('verifyToken').find(params);
       expect.assertions(1);
       expect(result).toBeTruthy();
-    });
-
-    it('Should error on undefined token', async () => {
-      const params = {
-        query: {
-          test: '12345',
-        },
-      };
-      const result = await app.service('verifyToken').find(params);
-      expect.assertions(1);
-      expect(result).toBeFalsy();
-    });
-
-    it('Should error if called from get', async () => {
-      const params = {
-        query: {
-          test: '12345',
-        },
-      };
-      const result = await app.service('verifyToken').get(params);
-      expect.assertions(1);
-      expect(result.name).toBe('NotFound');
     });
   });
 });
