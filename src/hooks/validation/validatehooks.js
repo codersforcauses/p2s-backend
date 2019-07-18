cad7613a3d34fe6a825e7dd40bfe5f13f467e20a
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-07-18 23:01:04 +0800

    Added joi error naming

--- src/hooks/validation/validatehooks.js
+++ src/hooks/validation/validatehooks.js
@@ -9,7 +9,16 @@ module.exports = {
   validateSchema: schema => async (context) => {
     schema.validate(context.data,
       (err) => {
-        if (err) throw new BadRequest(err);
+        if (err) {
+          if (err.isJoi) {
+            const [{ message }] = err.details;
+            if (message === 'emailError') throw new BadRequest('Invalid email');
+            else if (message === 'passError') throw new BadRequest('Invalid password');
+            else if (message === 'strategyError') throw new BadRequest('Invalid strategy');
+            else if (message === 'accessTokenError') throw new BadRequest('Invalid access token');
+            else throw new BadRequest(err);
+          }
+        }
       });
     return context;
   },
